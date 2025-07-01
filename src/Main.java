2460fc41590efe7e608c6271cbf51a6cd8712ebd
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-01 08:49:33 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // This is my first java programm
+
+
+        System.out.println("Hello, world!");
+        System.out.println("This is new world");
+
+        /*
+        variable are of two types first is primitive and other is  reference.
+        types of datatype 1 int
+                          2 double
+                          3 char
+                          4 boolean
+                          5 string
+                          6 array
+                          7 object
+        steps to creat integer are
+        1 declaration
+        2 assignment
+
+        */
+
+        int age;
+        age = 21;
+        int year = 2025;
+        System.out.println(age);
+        System.out.println("The year is "+ year);
+
+
+    }
+}
