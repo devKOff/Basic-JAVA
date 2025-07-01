2f9013cfd6241dad984c064bdf199bf2079d8e7f
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-01 10:45:54 +0530

    new initial commit

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,19 @@ public class Main {
         int age;
         age = 21;
         int year = 2025;
+
+
+        double quantity = 12.8;
+        double salary = 20000;
+        double cgpa = 6.9;
+
+        char gender = 'M';
+        char grade = 'F';
+
         System.out.println(age);
         System.out.println("The year is "+ year);
+        System.out.println("The quantity is "+ quantity);
+        System.out.println("My gender is "+ gender);
 
 
     }
