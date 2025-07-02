37eb4176491f0cb5bd77c26833140fb9fd2c669d
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-02 19:41:38 +0530

    FirstProject

--- src/MadLibsGame.java
+++ src/MadLibsGame.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 public class MadLibsGame {
     public static void main(String[] args) {
 
+        //
+        int age;
+        age = 21;
+        System.out.println("Hello, world!");
+        System.out.println("This is new world");
+
+
         Scanner sc = new Scanner(System.in);
 
 
