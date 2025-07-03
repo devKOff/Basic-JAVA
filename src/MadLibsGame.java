31d5b2dbd429ba3d9f7ba4d3bdfb06e7c85b33b5
Devendra Kashyap <dkashyap2105@gmail.com>
2025-07-03 19:18:38 +0530

    FirstProject

--- src/MadLibsGame.java
+++ src/MadLibsGame.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
 public class MadLibsGame {
     public static void main(String[] args) {
 
-        //
-        int age;
-        age = 21;
-        System.out.println("Hello, world!");
-        System.out.println("This is new world");
-
-
         Scanner sc = new Scanner(System.in);
 
 
+        String adjective1;
+        String adjective2;
+        String noun1;
+        String noun2;
+        String verb1;
 
+        System.out.print("Enter adjective 1: ");
+        adjective1 = sc.nextLine();
+        System.out.print("Enter adjective 2: ");
+        adjective2 = sc.nextLine();
+        System.out.print("Enter noun1: ");
+        noun1 = sc.nextLine();
+        System.out.print("Enter noun2: ");
+        noun2 = sc.nextLine();
+        System.out.print("Enter verb1: ");
+        verb1 = sc.nextLine();
+
+        System.out.print(verb1 + " both are animals but " + noun1 +" runs " +adjective1 + " and " + noun2 +" runs "+ adjective2 );
+
+        sc.close();
     }
 }
